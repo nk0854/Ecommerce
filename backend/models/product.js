1410fd7451d74df77eabe6dd528cea2b1c4a679a
nk0854 <72222010+nk0854@users.noreply.github.com>
2023-09-19 12:06:25 +0530

    product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,60 +1,62 @@
-const mongoose=require('mongoose')
-const productSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    richDescription:{
-        type:String,
-        default:''
+    richDescription: {
+        type: String,
+        default: ''
     },
-    image:{
-        type:String,
-        default:''
+    image: {
+        type: String,
+        default: ''
     },
-    images:[{
-        type:String
+    images: [{
+        type: String
     }],
-    brand:{
-        type:String,
-        default:''
+    brand: {
+        type: String,
+        default: ''
     },
-    price:{
-        type:Number,
+    price : {
+        type: Number,
         default:0
     },
-    category:{
+    category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'Category',
+        ref: 'Category',
         required:true
     },
-    countInStock:{
-        type:Number,
-        required:true,
-        min:0,
-        max:255
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
     },
-    rating:{
-        type:Number,
-        default:0
+    rating: {
+        type: Number,
+        default: 0,
     },
     numReviews: {
         type: Number,
         default: 0,
     },
-    isFeatured:{
-        type:Boolean,
-        default:false
+    isFeatured: {
+        type: Boolean,
+        default: false,
     },
-    dateCreated:{
-        type:Date,
-        default:Date.now
-    }
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
 })
+
 productSchema.virtual('id').get(function () {
     return this._id.toHexString();
 });
@@ -64,4 +66,4 @@ productSchema.set('toJSON', {
 });
 
 
-exports.Product=mongoose.model('Product',productSchema);
+exports.Product = mongoose.model('Product', productSchema);
